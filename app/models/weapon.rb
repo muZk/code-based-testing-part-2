01d720609bdf6fd7efb95527213f24e506c7a077
muzk <nggomez@uc.cl>
2014-09-29 17:23:15 -0300

    #1 listo, se reemplazo create y create!

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,15 +1,45 @@
 class Weapon < ActiveRecord::Base
-    has_many :robot_weapons 
-    has_many :robots, through: :robot_weapons
+  has_many :robot_weapons
+  has_many :robots, through: :robot_weapons
 
-    # should be uniq by name - 
-    # if created with the same name as one previously saved should return that one 
+  # should be uniq by name -
+  # if created with the same name as one previously saved should return that one
 
-    # validates :name, uniqueness: true
-    # validate :get_instance_by_name 
+  # validates :name, uniqueness: true
+  # validate :get_instance_by_name
 
-    def get_instance_by_name
-    	# return the existing object if any, otherwise return true
+  def get_instance_by_name
+    # return the existing object if any, otherwise return true
+  end
+
+  def self.create(attributes, &block)
+    if attributes.is_a?(Array)
+      attributes.collect { |attr| create(attr, &block) }
+    else
+      if attributes.has_key?(:name)
+        weapon = find_by(name: attributes[:name])
+        return weapon if weapon.present?
+      end
+      object = new(attributes, &block)
+      object.save
+      object
     end
+  end
+
+  def self.create!(attributes, &block)
+    if attributes.is_a?(Array)
+      attributes.collect { |attr| create!(attr, &block) }
+    else
+      if attributes.has_key?(:name)
+        weapon = find_by(name: attributes[:name])
+        return weapon if weapon.present?
+      end
+      object = new(attributes)
+      yield(object) if block_given?
+      object.save!
+      object
+    end
+  end
+
 
 end
