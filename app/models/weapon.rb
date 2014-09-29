394d2c1659125185bae151a04c3ce25c767c578f
muzk <nggomez@uc.cl>
2014-09-29 18:34:19 -0300

    #1 ahora SI que SI funciona, reemplace el funcionamiento de new_record? :)

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -8,38 +8,60 @@ class Weapon < ActiveRecord::Base
   # validates :name, uniqueness: true
   # validate :get_instance_by_name
 
+  before_create :lawl
+
+  def initialize(attributes = nil, options = {})
+    puts ':o'
+    super
+  end
+
+  def lawl
+    puts ':O'
+  end
+
   def get_instance_by_name
     # return the existing object if any, otherwise return true
   end
 
-  def self.create(attributes, &block)
-    if attributes.is_a?(Array)
-      attributes.collect { |attr| create(attr, &block) }
-    else
-      if attributes.has_key?(:name)
-        weapon = find_by(name: attributes[:name])
-        return weapon if weapon.present?
-      end
-      object = new(attributes, &block)
-      object.save
-      object
-    end
-  end
+  # def self.create(attributes, &block)
+  #   if attributes.is_a?(Array)
+  #     attributes.collect { |attr| create(attr, &block) }
+  #   else
+  #     if attributes.has_key?(:name)
+  #       weapon = find_by(name: attributes[:name])
+  #       return weapon if weapon.present?
+  #     end
+  #     object = new(attributes, &block)
+  #     object.save
+  #     object
+  #   end
+  # end
+  #
+  # def self.create!(attributes, &block)
+  #   if attributes.is_a?(Array)
+  #     attributes.collect { |attr| create!(attr, &block) }
+  #   else
+  #     if attributes.has_key?(:name)
+  #       weapon = find_by(name: attributes[:name])
+  #       return weapon if weapon.present?
+  #     end
+  #     object = new(attributes)
+  #     yield(object) if block_given?
+  #     object.save!
+  #     object
+  #   end
+  # end
 
-  def self.create!(attributes, &block)
-    if attributes.is_a?(Array)
-      attributes.collect { |attr| create!(attr, &block) }
-    else
-      if attributes.has_key?(:name)
-        weapon = find_by(name: attributes[:name])
-        return weapon if weapon.present?
+  def new_record?
+    if self.name.present?
+      weapon = Weapon.find_by(name: self.name)
+      if weapon.present?
+        self.id = weapon.id
+        self.created_at = weapon.created_at
+        return false
       end
-      object = new(attributes)
-      yield(object) if block_given?
-      object.save!
-      object
     end
+    super
   end
-
 
 end
--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 RSpec.describe Weapon, :type => :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
-  context "Factories" do 
-    it ":health" do 
+  context "Factories" do
+    it ":health" do
       gun = FactoryGirl.create(:gun)
       expect(gun.id).to be > 0
     end
 
-    it ":health_800" do 
+    it ":health_800" do
       gun = FactoryGirl.create(:rifle)
       expect(gun.id).to be > 0
     end
 
-    it ":health_1000" do 
+    it ":health_1000" do
       gun = FactoryGirl.create(:machine_gun)
       expect(gun.id).to be > 0
     end
 
-    it ":health_x" do 
+    it ":health_x" do
       gun = FactoryGirl.create(:bazuka)
       expect(gun.id).to be > 0
     end
 
-    it "should not create two different instances for the same (defined) Weapon" do 
+    it "should not create two different instances for the same (defined) Weapon" do
       gun1 = FactoryGirl.create(:bazuka)
       gun2 = FactoryGirl.create(:bazuka)
-      # expect(gun1.id == gun2.id).to be true
+      expect(gun1.id == gun2.id).to be true
     end
   end
 
